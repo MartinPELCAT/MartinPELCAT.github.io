a3e28818b784305de9c805fdeb9a1fcb97b50535
Martin PELCAT <martinpelcat@hotmail.com>
2021-02-21 16:46:49 +0100

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "54f125e06eb6d0ca218b";
+/******/ 	var hotCurrentHash = "72ffcabe5fc9ab3dfd4a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
