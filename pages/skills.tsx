674e9a5d3264cd245e3908ce371562b00d5620b6
Martin PELCAT <martinpelcat@hotmail.com>
2021-04-15 18:56:21 +0200

    add skills

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -3,6 +3,59 @@ import dynamic from "next/dynamic";
 const Layout = dynamic(() => import("../components/Layout"));
 
 const Skills = () => {
-  return <Layout>Skills</Layout>;
+  return (
+    <Layout>
+      <div className="divide-y space-y-10 px-2">
+        <div className="max-w-2xl pt-5 mx-auto grid grid-cols-3 md:grid-cols-5 gap-3">
+          <div className="col-span-3 md:col-span-5 font-bold text-3xl">
+            Core
+          </div>
+          <SkillItem image="/next-js.svg" text="NextJS" />
+          <SkillItem image="/react.png" text="ReactJS" />
+          <SkillItem image="/graphql.png" text="GraphQL" />
+          <SkillItem image="/tailwindcss.png" text="TailwindCSS" />
+          <SkillItem image="/typescript.png" text="Typescript" />
+          <SkillItem image="/nodejs.png" text="NodeJS" />
+          <SkillItem image="/javascript.png" text="Javascript" />
+        </div>
+        <div className="max-w-2xl pt-5 mx-auto grid grid-cols-3 md:grid-cols-5 gap-3">
+          <div className="col-span-3 md:col-span-5 font-bold text-3xl">
+            Autres
+          </div>
+          <SkillItem image="/postgresql.png" text="PostgreSQL" />
+          <SkillItem image="/git.png" text="GIT" />
+          <SkillItem image="/java.png" text="JAVA" />
+          <SkillItem image="/hibernate.png" text="Hibernate" />
+          <SkillItem image="/spring.png" text="Spring" />
+          <SkillItem image="/laravel.png" text="Laravel" />
+          <SkillItem image="/symfony.png" text="Symfony" />
+          <SkillItem image="/rest.png" text="RestAPI" />
+          <SkillItem image="/csharp.png" text="C#" />
+          <SkillItem image="/mysql.png" text="MySql" />
+          <SkillItem image="/oracle.png" text="Oracle" />
+          <SkillItem image="/OSGI.png" text="OSGI" />
+          <SkillItem image="/php.png" text="Php" />
+          <SkillItem image="/python.png" text="Python" />
+          <SkillItem image="/jquery.gif" text="JQuery" />
+          <SkillItem image="/office.png" text="Office" />
+          <SkillItem image="/anglais.png" text="Anglais" />
+        </div>
+      </div>
+    </Layout>
+  );
 };
+
+interface Props {
+  text: string;
+  image: string;
+}
+const SkillItem = ({ image, text }: Props) => {
+  return (
+    <div className="bg-white rounded-md shadow flex py-3 flex-col justify-between">
+      <img src={`/skills${image}`} alt="" className="px-3" />
+      <div className="text-center font-light italic pt-3">{text}</div>
+    </div>
+  );
+};
+
 export default Skills;
--- public/sw.js
+++ public/sw.js
@@ -1,44 +1,44 @@
 if (!self.define) {
   const e = (e) => {
       "require" !== e && (e += ".js");
-      let i = Promise.resolve();
+      let s = Promise.resolve();
       return (
-        s[e] ||
-          (i = new Promise(async (i) => {
+        a[e] ||
+          (s = new Promise(async (s) => {
             if ("document" in self) {
-              const s = document.createElement("script");
-              (s.src = e), document.head.appendChild(s), (s.onload = i);
-            } else importScripts(e), i();
+              const a = document.createElement("script");
+              (a.src = e), document.head.appendChild(a), (a.onload = s);
+            } else importScripts(e), s();
           })),
-        i.then(() => {
-          if (!s[e]) throw new Error(`Module ${e} didn’t register its module`);
-          return s[e];
+        s.then(() => {
+          if (!a[e]) throw new Error(`Module ${e} didn’t register its module`);
+          return a[e];
         })
       );
     },
-    i = (i, s) => {
-      Promise.all(i.map(e)).then((e) => s(1 === e.length ? e[0] : e));
+    s = (s, a) => {
+      Promise.all(s.map(e)).then((e) => a(1 === e.length ? e[0] : e));
     },
-    s = { require: Promise.resolve(i) };
-  self.define = (i, n, a) => {
-    s[i] ||
-      (s[i] = Promise.resolve().then(() => {
-        let s = {};
-        const t = { uri: location.origin + i.slice(1) };
+    a = { require: Promise.resolve(s) };
+  self.define = (s, n, i) => {
+    a[s] ||
+      (a[s] = Promise.resolve().then(() => {
+        let a = {};
+        const r = { uri: location.origin + s.slice(1) };
         return Promise.all(
-          n.map((i) => {
-            switch (i) {
+          n.map((s) => {
+            switch (s) {
               case "exports":
-                return s;
+                return a;
               case "module":
-                return t;
+                return r;
               default:
-                return e(i);
+                return e(s);
             }
           })
         ).then((e) => {
-          const i = a(...e);
-          return s.default || (s.default = i), s;
+          const s = i(...e);
+          return a.default || (a.default = s), a;
         });
       }));
   };
@@ -51,96 +51,96 @@ define("./sw.js", ["./workbox-a8b10d99"], function (e) {
     e.precacheAndRoute(
       [
         {
-          url: "/_next/static/Pqtmehiz3pkMVEW3Lo1Vi/_buildManifest.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/1RZEG6St-rje0dk08aHPA/_buildManifest.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/Pqtmehiz3pkMVEW3Lo1Vi/_ssgManifest.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/1RZEG6St-rje0dk08aHPA/_ssgManifest.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/18.5dd5c495d9b4afafd2fe.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/chunks/4.53f9c80a7ec04067e321.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/chunks/4.dfe9fade2fc998383c07.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/6.46dd4e6be3600a3cabcc.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/7.7729363912f91dd9afad.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url:
-            "/_next/static/chunks/a621c6d6b0e0f3d9c286172de904f218404abe89.c9025eb4fa00bead3af6.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+            "/_next/static/chunks/a621c6d6b0e0f3d9c286172de904f218404abe89.c85ede1a2055f7e37e79.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url:
             "/_next/static/chunks/b3fde4e130b99ca7088fe256fb8c354e6136b697.a9c267cac26afd58f8d0.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url:
             "/_next/static/chunks/ee9974e4832feb30b1f7a035081edf935c5b1feb.b78c70d5dc58f07d7ffe.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/framework.f9803f2774bc67c45cde.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/main-030d69f610e11ddcda55.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/chunks/pages/_app-b35e1aefcee41aae0eb1.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/chunks/pages/_app-b599f680c73161183db5.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/pages/_error-ea4b755d6e3bc2471140.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/pages/contact-348646bcddf26a4703ab.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/pages/diplomas-e7b6a0a7a0a1f30ab63c.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url:
             "/_next/static/chunks/pages/experience/%5Bid%5D-2493d0c3ab10dcdf451f.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/pages/experiences-fc0837b11c02a561790d.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/chunks/pages/index-6b3d668ab3a1f2820c87.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/chunks/pages/index-49c908968938efd5a08a.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/pages/skills-13f8aaa6569c75fc30d0.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
           url: "/_next/static/chunks/polyfills-79441a336eb07a143a5d.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/chunks/webpack-321cc46478932614d577.js",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/chunks/webpack-d35ca221132433de568a.js",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         {
-          url: "/_next/static/css/fdb837b63ae594983413.css",
-          revision: "Pqtmehiz3pkMVEW3Lo1Vi",
+          url: "/_next/static/css/d47794a6b00f8bf1a2f7.css",
+          revision: "1RZEG6St-rje0dk08aHPA",
         },
         { url: "/favicon.ico", revision: "f1231392d9b0fb70407c11b64e14f09f" },
         {
@@ -191,6 +191,7 @@ define("./sw.js", ["./workbox-a8b10d99"], function (e) {
           url: "/images/logotetras.png",
           revision: "815ca9fff6144ff04b62cba946a05912",
         },
+        { url: "/images/me.JPG", revision: "7952ea1c33d6e6f9a14933d66fd5a2d0" },
         {
           url: "/images/overkiz.png",
           revision: "9b685a4f4ab8c05a2c8deb605855f13a",
